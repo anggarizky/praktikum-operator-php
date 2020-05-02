bf069b7a31264a008ced33421d611fc8317f8e1a
anggarizky <63494073+anggarizky@users.noreply.github.com>
2020-05-02 12:58:56 +0700

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,15 @@
+<?php
+//ini adalah file pertama menggunakan php
+
+$a =10;
+$b ="saya adalah PHP";
+
+//sekarang kita panggil variabel di atas 
+/*dengan cara mencetak ke browser agar kita dapat melihat 
+-menggunakan perintah echo*/
+
+echo "ini adalah variabel A :".$a."<br>";
+echo "ini adalah variabel B $b";
+
+
+?>
